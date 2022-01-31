1247d684edba2828d0738ef66d733c08210c676d
Roman Munar <roman.munar@chromedia.com>
2022-01-31 18:53:57 +0800

    reproduced Unsupported URL Type "workspace:": workspace:*

---
+++ apps/api/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var test_1 = require("./test");
+(0, test_1.test)();
---
+++ apps/api/dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.test = void 0;
+var test = function () {
+    console.log('TESt');
+};
+exports.test = test;
